61f0ee9b282bcce4b878e569fadc344501625237
sami0312 <samizarroug03@gmail.com>
2025-09-05 22:48:31 +0100

    Initial commit with project files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/clone-drive-school/',
   server: {
     host: "::",
     port: 8080,
